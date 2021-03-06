6f839153f383a63194fba8487d9c0d1b7e9f8e34
Gabriel Umbelino <gabriel.umbelino@gmail.com>
2021-03-06 22:01:35 +0100

    update person page

--- src/pages/person/person.vue
+++ src/pages/person/person.vue
@@ -1,12 +1,18 @@
 <template>
+  <section class="hero is-primary has-text-centered">
+    <div class="hero-body">
+      <p class="title">Cadastro de Pessoa</p>
+      <p class="subtitle">Preencha o formulário abaixo.</p>
+    </div>
+  </section>
   <div class="container is-max-desktop person">
     <PersonRegister />
   </div>
 </template>
 
 <script lang="ts">
-  import { PersonRegister } from '@/components'
   import { defineComponent } from 'vue'
+  import { PersonRegister } from '@/components/person/person-register'
 
   export default defineComponent({
     components: { PersonRegister },
@@ -17,7 +23,16 @@
   })
 </script>
 
-<style lang="sass">
-  .person
-    background: white
+<style lang="scss">
+  .hero {
+    .hero-body {
+      width: $form-width;
+      max-width: 100%;
+      margin: auto;
+      padding: 30px 0px;
+    }
+  }
+  .person {
+    background: white;
+  }
 </style>
